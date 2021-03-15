df72221e858e116a0bc890a7ed98018a0dc8e291
mcmkstankiewicz <mcmkstankiewicz@gmail.com>
2021-03-15 21:17:07 +0100

    Dodaj pliki projektów.

---
+++ AddressBK/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBK.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica:")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage = "Pole jest wymagane")]
+        public string Street { get; set; }
+        [Display(Name = "jej numer:")]
+        public int Number { get; set; }
+        [Display(Name = "i kod pocztowy:")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Znajduje się w mieście:")]
+        public string City { get; set; }
+    }
+}
